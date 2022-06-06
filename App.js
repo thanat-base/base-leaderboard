c2cb394395f2e65e2a0601a25472101cd1784e23
thanatraktham <thanat.sun19821@gmail.com>
2022-06-06 16:07:21 +0700

    import fonts

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import { Platform, StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 import WebLandingScreen from "./src/web/screens/WebLandingScreen";
 import MobileLandingScreen from "./src/mobile/screens/MobileLandingScreen";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "Prompt-Bold": require("./src/assets/fonts/Prompt/Prompt-Bold.ttf"),
+    "Prompt-Light": require("./src/assets/fonts/Prompt/Prompt-Light.ttf"),
+    "Prompt-Regular": require("./src/assets/fonts/Prompt/Prompt-Regular.ttf"),
+  });
+
+  if (fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === "web" && <WebLandingScreen />}
+        {(Platform.OS === "android" || Platform.OS === "ios") && (
+          <MobileLandingScreen />
+        )}
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
   return (
-    <View style={styles.container}>
-      {Platform.OS === "web" && <WebLandingScreen />}
-      {(Platform.OS === "android" || Platform.OS === "ios") && (
-        <MobileLandingScreen />
-      )}
-      <StatusBar style="auto" />
+    <View>
+      <Text style={{ fontFamily: "Prompt-Bold" }}>Default Screen</Text>
     </View>
   );
 }
---
+++ src/components/MyButton.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MyButton = () => {
+  return (
+    <View>
+      <Text>MyButton</Text>
+    </View>
+  );
+};
+
+export default MyButton;
+
+const styles = StyleSheet.create({});
